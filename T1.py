68bbf0bc9a9163f8783ef86034a097826e2a9104
UbuntuImage_May2021 <mike.hofmann.student@gmail.com>
2025-10-26 19:56:49 -0700

    Initial commit

---
+++ T1.py
@@ -0,0 +1,19 @@
+# this is a string
+name = "Peter"
+text = "I love python"
+print(name)
+print(text)
+
+# this is an integer
+age = 23
+print(age)
+
+# this is a float
+height = 1.81
+print(height)
+
+# printing with text and variables
+print(f"Hello I am {name} and {text}")
+print(f"I am {age} years old and {height} m tall")
+
+
