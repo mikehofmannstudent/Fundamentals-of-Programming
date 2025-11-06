3200cb1e9c5c8abb982548a866e7150a00cebba9
unknown <mike.hofmann.student@gmail.com>
2025-11-06 16:57:23 +0800

    Finished Exercises

--- Week2/exercises/E12.py
+++ Week2/exercises/E12.py
@@ -1,14 +1,21 @@
 print("\nBUCKET LIST BUILDER\n")
 bucket = []
-choice = input("Enter selection (e(X)it, (A)dd, (L)ist): ")
+choice = input("Enter selection (e(X)it, (A)dd, (D)elete, (L)ist): ").upper()
 while choice [0] != "X":
     if choice[0] == "A":
         print("Enter list item: ")
         newitem = input()
         bucket.append(newitem)
+    elif choice[0] == "D":
+        print("Enter item to delete: ")
+        delItem = input()
+        if delItem in bucket:
+            bucket.remove(delItem)
+        else:
+            print("Item is not in list. (Check spelling)")
     elif choice[0] == "L":
         for item in bucket:
             print(item)
     else:
         print("Invalid selection.")
-    choice = input("Enter selection (e(X)it, (A)dd, (L)ist):")
+    choice = input("Enter selection (e(X)it, (A)dd, (D)elete, (L)ist):").upper()
---
+++ Week2/exercises/E13.py
@@ -0,0 +1,4 @@
+import random
+
+biscuits = ['Monte Carlo'] * 7 + ['Shortbread Cream'] * 7 
+    + ['Delta Cream'] * 6 + ['Orange Slice'] * 6 + ['Kingston'] * 5
